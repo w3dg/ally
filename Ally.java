582b9263aac745cc00cdeea64cad3ab9f48ccfb7
w3dg <51162936+w3dg@users.noreply.github.com>
2022-05-23 12:39:38 +0530

    Adding Array printers

---
+++ Ally.java
@@ -0,0 +1,30 @@
+package ally;
+
+public class Ally {
+
+  // print elements of arrays
+  public static void printArray(int[] arr) {
+    for (int x : arr)
+      System.out.println(x);
+  }
+
+  public static void printArray(String[] arr) {
+    for (String x : arr)
+      System.out.println(x);
+  }
+
+  public static void printArray(float[] arr) {
+    for (float x : arr)
+      System.out.println(x);
+  }
+
+  public static void printArray(double[] arr) {
+    for (double x : arr)
+      System.out.println(x);
+  }
+
+  public static void printArray(char[] arr) {
+    for (char x : arr)
+      System.out.println(x);
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ally
+
+My (and maybe yours) Ally (helper), for mundane java debugging / printing tasks.
+
+> Very preliminary, only for me, i am learning.
